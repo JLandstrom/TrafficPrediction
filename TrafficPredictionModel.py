e6f00e0b3e286a84ad7b6ebe2a6e9c4be1324deb
Patric Linderoth <s153364@student.hb.se>
2018-01-25 12:23:31 +0100

    remove dates with not enough data in preprocess 2

--- TrafficPredictionModel.py
+++ TrafficPredictionModel.py
@@ -157,8 +157,8 @@ if __name__ == "__main__":
         dataset = dataFrame = pd.read_csv(outputFile, sep=";", decimal=",", encoding="utf-8", header=0, low_memory=False)
 
 
-    dataset = preprocessor.RemoveDays(dataset, 1000)
-    dataset = preprocessor.PreProcess(dataset, filePath=CreateCsvFilePath("Aggregated5MinIntervals", detectorIds))
+    #dataset = preprocessor.RemoveDays(dataset, 1000)
+    dataset = preprocessor.PreProcess(dataset, filePath=CreateCsvFilePath("Aggregated5MinIntervals", detectorIds),threshold=1000)
     dataset = preprocessor.Cluster(dataset)
     ArimaForecasting(dataset)
     # plotClusters(dataset, clusters, pd.datetime(2014,1,22).date())
